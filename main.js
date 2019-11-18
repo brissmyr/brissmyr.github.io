96f01873c75df75f87fb07f5523e7ecc6cb9da6b
Johan Brissmyr <brissmyr@gmail.com>
2019-11-17 19:21:13 -0800

    ReCAPTCHA v2

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 $(function () {
-  routes.call('send_code', {}, function (data) {
-    // alert(data)
-  });
+  Verify.ready(function() { 
+    Verify.on('captcha', function() {
+      $('.factor-captcha').show()
 
-  var form = document.getElementById('code-form');
-
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    var code = document.getElementById('code-field').value
-
-    routes.call('verify_code', { code: code }, function (data) {
-      console.log(data)
+      window.reCaptchaCallback = function(key) {
+        Verify.call('verify_recaptcha', {key: key}, function (data) {
+          // TBD: can we omit callback here?
+        });
+      }
     });
+    
+    Verify.on('sms', function() {
+      $('.factor-sms').show()
+      
+      Verify.call('send_sms', {}, function (data) {
+        // TBD: can we omit callback here?
+      });
+    
+      var form = document.getElementById('code-form');
+    
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+    
+        var code = document.getElementById('code-field').value
+    
+        Verify.call('verify_sms', { code: code }, function (data) {
+          if (data.success) {
+            console.log('Correct code. Redirecting...');
+          } else {
+            console.log('Wrong code. Please try again.');
+          }
+        });
+      });
+    });
+    
   });
-})
+});
